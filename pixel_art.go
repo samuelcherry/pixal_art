d7c45430db5cc88f7e45a258b7ce89f7046767ed
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-11-29 19:43:14 -0700

    fixed sync issue

--- pixel_art.go
+++ pixel_art.go
@@ -1,103 +1,241 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image"
 	"image/color"
 	_ "image/jpeg"
 	"image/png"
+	"io"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 )
 
+var inputFile string
 
-
-func main() {
+func init() {
 
 	dir := "./images"
-	newDir := "./colorBlocks"
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Printf("Error reading directoryL %v\n", err)
+		return
+	}
+	fmt.Println("function starts at: ", dir)
+	if len(entries) == 0 {
+		fmt.Println("dir not empty")
+
+		inputDir := "./input"
+
+		files,err := os.ReadDir(inputDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+
+		for _, f := range files {
+			if !f.IsDir() {
+				inputFile = filepath.Join(inputDir, f.Name())
+				break
+			}
+		}
+
+		if inputFile == "" {
+			log.Fatal("No input video fount in ./input folder")
+		}
+
+
+		cmd := exec.Command(
+		"C:/ffmpeg/bin/ffmpeg.exe",
+		"-i", inputFile,
+		"./images/%04d.png",
+		)
+
+		// FFmpeg sends progress to stderr
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(stderr)
+		frameRe := regexp.MustCompile(`frame=\s*(\d+)`)
+
+		fmt.Println("Extracting frames...")
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			if matches := frameRe.FindStringSubmatch(line); matches != nil {
+				frameNum, _ := strconv.Atoi(matches[1])
+				fmt.Printf("\rFrames processed: %d", frameNum)
+			}
+		}
+
+		if err := cmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("\nDone!")
+	}
+}
+
+func main() {
+//input dir, output dir
+	dir := "C:/Users/samue/Desktop/go_projects/pixal_art/images"
+	newDir := "C:/Users/samue/Desktop/go_projects/pixal_art/colorBlocks"
 
 	entries, err := os.ReadDir(dir)
 	if err != nil{
 		log.Fatal(err)
 	}
-	i := 0
-	for _, entry := range entries {
 
-		if entry.IsDir() {
-			continue
-		}
+	newEntries, err := os.ReadDir(newDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	if len(newEntries) == 0{
+		for index, entry := range entries {
 
-		i++
+			if entry.IsDir() {
+				continue
+			}
 
-		inputFile := filepath.Join(dir, entry.Name())
-		f, err := os.Open(inputFile)
-		if err != nil {
-			log.Fatal("Could not open input.jpg:", err)
-		}
-		defer f.Close()
+			inputFile := filepath.Join(dir, entry.Name())
+			f, err := os.Open(inputFile)
+			if err != nil {
+				log.Fatal("Could not open input.jpg:", err)
+			}
+			defer f.Close()
 
-		img, _, err := image.Decode(f)
-		if err != nil {
-			log.Fatal("Image decode failed:", err)
-		}
+			img, _, err := image.Decode(f)
+			if err != nil {
+				log.Fatal("Image decode failed:", err)
+			}
 
-		res := 64
+			res := 16
 
-		width := img.Bounds().Dx()
-		height := img.Bounds().Dy()
+			width := img.Bounds().Dx()
+			height := img.Bounds().Dy()
 
-		gridX := width/res
-		gridY := height/res
+			gridX := width/res
+			gridY := height/res
 
-		cellW := width/gridX
-		cellH := height/gridY
+			cellW := width/gridX
+			cellH := height/gridY
 
-		out := image.NewRGBA(image.Rect(0,0,width,height))
+			out := image.NewRGBA(image.Rect(0,0,width,height))
 
-		for gy := range gridY {
-			for gx := range gridX{
+			for gy := range gridY {
+				for gx := range gridX{
 
-				startX := gx * cellW
-				startY := gy * cellH
-				endX := startX + cellW
-				endY := startY + cellH
+					startX := gx * cellW
+					startY := gy * cellH
+					endX := startX + cellW
+					endY := startY + cellH
 
-				var rSum, gSum, bSum uint64
-				var count uint64
+					var rSum, gSum, bSum uint64
+					var count uint64
 
-				for y:=startY; y < endY; y++{
-					for x:= startX; x < endX; x++{
-						r,g,b,_ := img.At(x,y).RGBA()
-						rSum += uint64(r)
-						gSum += uint64(g)
-						bSum += uint64(b)
-						count++
+					for y:=startY; y < endY; y++{
+						for x:= startX; x < endX; x++{
+							r,g,b,_ := img.At(x,y).RGBA()
+							rSum += uint64(r)
+							gSum += uint64(g)
+							bSum += uint64(b)
+							count++
+						}
 					}
-				}
 
-				avgColor := color.RGBA{
-					uint8((rSum/count) >> 8),
-					uint8((gSum/count) >> 8),
-					uint8((bSum/count) >> 8),
-					255,
-				}
+					avgColor := color.RGBA{
+						uint8((rSum/count) >> 8),
+						uint8((gSum/count) >> 8),
+						uint8((bSum/count) >> 8),
+						255,
+					}
 
-				for y := startY; y < endY; y++ {
-					for x := startX; x < endX; x++ {
-						out.Set(x,y,avgColor)
+					for y := startY; y < endY; y++ {
+						for x := startX; x < endX; x++ {
+							out.Set(x,y,avgColor)
+						}
 					}
 				}
 			}
+
+			newImageName := fmt.Sprintf("%04d.png", index)
+			newImagePath := filepath.Join(newDir, newImageName)
+
+			
+			outFile, _ := os.Create(newImagePath)
+			defer outFile.Close()
+
+			png.Encode(outFile, out)
+
+			percent := int(float64(index)/float64(len(entries))*100)
+
+			fmt.Printf("%d%%\n", percent)
+			time.Sleep(50 * time.Millisecond)
 		}
-
-		newImageName := fmt.Sprintf("%04d.png", i)
-		newImagePath := filepath.Join(newDir, newImageName)
-
-		outFile, _ := os.Create(newImagePath)
-		defer outFile.Close()
-
-		png.Encode(outFile, out)
+		fmt.Println("\nDone!")
 	}
+	fmt.Println("\nExporting Video!")
+
+	base := filepath.Base(inputFile)
+
+		
+	fmt.Println("base string:", base)
+
+	name := strings.TrimSuffix(base, filepath.Ext(base))
+
+	fmt.Println("base string:", base)
+
+	outputPath := filepath.Join("./output", fmt.Sprintf("%s_16res.gif", name))
+
+	cmd := exec.Command(
+	"C:/ffmpeg/bin/ffmpeg.exe",
+	"-framerate", "24",
+	"-i", "colorBlocks/%04d.png",
+	outputPath,
+	)
+
+	stderr, err := cmd.StderrPipe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	slurp, _ := io.ReadAll(stderr)
+	fmt.Printf("%s\n", slurp)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.RemoveAll("./images")
+	os.Mkdir("./images", 0755)
+	os.RemoveAll("./colorBlocks")
+	os.Mkdir("./colorBlocks", 0755)
+
+	fmt.Println("\nFinished Exporting!")
+
 }
+
+
---
+++ script.js
@@ -0,0 +1,91 @@
+let names = [];
+
+async function loadNames() {
+  try {
+    const res = await fetch("options.json");
+    const data = await res.json();
+    names = data.names;
+  } catch (e) {
+    console.error("Cound not load JSON, using fallback array.");
+    names = ["See you", "Space Cowboy"];
+  }
+  setupAutocomplete("nameInput", names);
+}
+document.addEventListener("DOMContentLoaded", loadNames);
+
+function setupAutocomplete(inputId, list = []) {
+  const input = document.getElementById(inputId);
+  let currentFocus;
+
+  input.addEventListener("input", function () {
+    const val = this.value;
+    closeAllLists();
+    if (!val) return false;
+
+    const listContainer = document.createElement("div");
+    listContainer.setAttribute("id", this.id + "-autocomplete-list");
+    listContainer.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(listContainer);
+
+    let count = 0;
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].toLowerCase().includes(val.toLowerCase())) {
+        const item = document.createElement("div");
+        item.innerHTML = list[i];
+        item.setAttribute("class", "autocomplete-item");
+        item.addEventListener("click", function () {
+          input.value = this.innerText;
+          closeAllLists();
+        });
+        listContainer.appendChild(item);
+        count++;
+        if (count >= 10) break;
+      }
+    }
+  });
+
+  input.addEventListener("keydown", function (e) {
+    let list = document.getElementById(this.id + "-autocomplete-list");
+    if (list) list = list.getElementsByClassName("autocomplete-item");
+    if (!list) return;
+
+    if (e.key === "ArrowDown") {
+      currentFocus++;
+      addActive(list);
+    } else if (e.key === "ArrowUp") {
+      currentFocus--;
+      addActive(list);
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      if (currentFocus > -1) list[currentFocus].click();
+    }
+  });
+
+  function addActive(list) {
+    if (!list) return false;
+    removeActive(list);
+    if (currentFocus >= list.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = list.length - 1;
+    list[currentFocus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(list) {
+    for (let item of list) {
+      item.classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    const items = document.getElementsByClassName("autocomplete-items");
+    for (let item of items) {
+      if (elmnt != item && elmnt != input) {
+        item.parentNode.removeChild(item);
+      }
+    }
+    currentFocus = -1;
+  }
+
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
